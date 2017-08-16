8ba6f1738b991e1f8fc2249f4f93bed33a9d69c3
Adam Taranto <adam.p.taranto@gmail.com>
2017-08-16 16:34:32 +1000

    Complete draft ready for testing!

--- exterminate.py
+++ exterminate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#python 2.7.5
+#python 3
 #exterminate.py
 #Version 1.0 Adam Taranto, August 2017
 #Contact, Adam Taranto, adam.taranto@anu.edu.au
@@ -13,14 +13,15 @@ import os
 import re
 import sys
 import shutil
-from datetime import datetime
 import argparse
-from collections import Counter
 from Bio import SeqIO
+from datetime import datetime
+from collections import Counter
 from pymummer import coords_file, alignment, nucmer
 
 def dochecks(args):
-	"""Make outDir if does not exist else set to current dir."""
+	"""Housekeeping tasks: Create output files/dirs and temp dirs as required."""
+	# Make outDir if does not exist else set to current dir.
 	if args.outdir:
 		absOutDir = os.path.abspath(args.outdir)
 		if not os.path.isdir(args.outdir):
@@ -28,21 +29,28 @@ def dochecks(args):
 		outDir = absOutDir
 	else:
 		outDir = os.getcwd() 
-
-	#Make temp directory
+	# Make temp directory
 	tempDir = os.path.join(os.getcwd(),"temp_" + getTimestring())
 	os.makedirs(tempDir)
-
-	return outDir,tempDir
+	# Set prefix to infile basename if none
+	if not args.prefix:
+		prefix = os.path.splitext(os.path.basename(args.infile))[0]
+	else:
+		prefix = args.prefix
+	# Create outfile paths
+	outfile = prefix + "_exterm_extracted.fasta"
+	outpath = os.path.join(outDir,outfile)
+	# Return full path to output file and temp directory
+	return outpath,tempDir
 
 def getTimestring():
-	"""..."""
+	"""Return int only string of current datetime with milliseconds."""
 	(dt, micro) = datetime.utcnow().strftime('%Y%m%d%H%M%S.%f').split('.')
 	dt = "%s%03d" % (dt, int(micro) / 1000)
 	return dt
 
 def checkUniqueID(records):
-	"""..."""
+	"""Check that IDs for input elements are unique."""
 	seqIDs = [records[x].id for x in range(len(records))]
 	IDcounts = Counter(seqIDs)
 	duplicates = [k for k, v in IDcounts.items() if v > 1]
@@ -53,8 +61,8 @@ def checkUniqueID(records):
 	else:
 		pass
 
-def manageTemp(record=None,tempPath=None,scrub=False):
-	"""..."""
+def manageTemp(record=None, tempPath=None, scrub=False):
+	"""Create single sequence fasta files or scrub temp files."""
 	if scrub and tempPath:
 		try:
 			os.remove(tempPath)
@@ -65,7 +73,7 @@ def manageTemp(record=None,tempPath=None,scrub=False):
 			SeqIO.write(record, f, "fasta")
 
 def importFasta(file):
-	"""..."""
+	"""Load elements from multifasta file. Check that seq IDs are unique."""
 	# Read in elements from multifasta file, convert seqrecord iterator to list.
 	records = list(SeqIO.parse(file, "fasta"))
 	# Check names are unique
@@ -74,13 +82,13 @@ def importFasta(file):
 	return records
 
 def cleanID(s):
-	"""..."""
+	"""Remove non alphanumeric characters from string. Replace whitespace with underscores."""
 	s = re.sub(r"[^\w\s]", '', s)
 	s = re.sub(r"\s+", '_', s)
 	return s
 
-def getLTRs(elements=None,flankdist=10,minterm=10,minid=90,report='split',temp=None):
-	"""..."""
+def getLTRs(elements=None, flankdist=10, minid=80, minterm=10, minseed=5, diagfactor=0.3, report='split', temp=None):
+	"""Align elements to self and attempt to identify LTRs."""
 	# Set temp directory to cwd if none.
 	if not temp:
 		temp = os.getcwd()
@@ -90,12 +98,17 @@ def getLTRs(elements=None,flankdist=10,minterm=10,minid=90,report='split',temp=N
 		tempFasta = os.path.join(temp, cleanID(rec.id) + '.fasta')
 		tempCoords = tempFasta + '.coords'
 		# Write current element to single fasta
-		manageTemp(record=rec,tempPath=tempFasta,scrub=False)
+		manageTemp(record=rec, tempPath=tempFasta, scrub=False)
 		# Compose Nucmer script for current element vs self
-		runner = nucmer.Runner(tempFasta, tempFasta, tempCoords,
-						min_id=minid, min_length=minterm, 
-						breaklen=100, maxmatch=True, 
-						simplify=False)
+		runner = nucmer.Runner(	tempFasta, tempFasta, tempCoords,
+								min_id		=	minid, 
+								min_length	=	minseed,
+								diagfactor	=	diagfactor,
+								mincluster	=	minterm,
+								breaklen	=	200,
+								maxmatch	=	True,
+								simplify	=	False
+								)
 		# Execute nucmer
 		runner.run()
 		# Import coords file to iterator object
@@ -106,30 +119,43 @@ def getLTRs(elements=None,flankdist=10,minterm=10,minid=90,report='split',temp=N
 		alignments = [hit for hit in alignments if hit.on_same_strand()]
 		# Filter for 5' repeats which begin within x bases of element start
 		alignments = [hit for hit in alignments if hit.ref_start <= flankdist]
+		# Filter for 5' repeats whose 3' match ends within x bases of element end
+		alignments = [hit for hit in alignments if len(rec) - hit.qry_end <= flankdist]
 		# Scrub overlappying ref / query segments, and also complementary 3' to 5' flank hits
 		alignments = [hit for hit in alignments if hit.ref_end < hit.qry_start]
 		# Sort largest to smallest dist between end of ref (subject) and start of query (hit)
-		# x.qry_start - x.ref_end = length of internal segment
+		# x.qry_start (3') - x.ref_end (5') = Length of internal segment
 		alignments = sorted(alignments, key=lambda x: (x.qry_start - x.ref_end), reverse=True)
 		# If alignments exist after filtering report features using alignment pair with largest 
 		# internal segment i.e. first element in sorted list.
 		if alignments:
 			if report == 'all':
+				# yield original element
 				yield rec
 			if report in ['split','external']:
-				yield rec[alignments[0].ref_start:alignments[0].ref_end] # yield LTR slice - append "_LTR"
+				# yield LTR slice - append "_LTR"
+				extSeg = rec[alignments[0].ref_start:alignments[0].ref_end]
+				extSeg.id = extSeg.id + "_LTR"
+				extSeg.name = extSeg.id
+				yield extSeg
 			if report in ['split','internal']:
-				yield rec[alignments[0].ref_end:alignments[0].qry_start] # yield internal slice - append "_I"
+				# yield internal slice - append "_I"
+				intSeg = rec[alignments[0].ref_end:alignments[0].qry_start] 
+				intSeg.id = intSeg.id + "_I"
+				intSeg.name = intSeg.id
+				yield intSeg
 		else:
 			# If alignment list empty after filtering print alert and continue
 			print('No LTRs found for candidate element: %s' % rec.id)
-			pass
 		# Scrub single fasta and coords file for current element.
-		manageTemp(tempPath=tempFasta,scrub=True)
-		manageTemp(tempPath=tempCoords,scrub=True)
+		manageTemp(tempPath=tempFasta, scrub=True)
+		manageTemp(tempPath=tempCoords, scrub=True)
 
-def getTIRs(elements=None,flankdist=10,minterm=10,minid=90,mites=False,report='split',temp=None):
-	"""..."""
+def getTIRs(elements=None, flankdist=10, minid=80, minterm=10, minseed=5, diagfactor=0.3, mites=False, report='split', temp=None):
+	""" 
+	Align elements to self and attempt to identify TIRs. 
+	Optionally attempt to construct synthetic MITEs from TIRs.
+	"""
 	# Set temp directory to cwd if none.
 	if not temp:
 		temp = os.getcwd()
@@ -139,12 +165,17 @@ def getTIRs(elements=None,flankdist=10,minterm=10,minid=90,mites=False,report='s
 		tempFasta = os.path.join(temp, cleanID(rec.id) + '.fasta')
 		tempCoords = tempFasta + '.coords'
 		# Write current element to single fasta
-		manageTemp(record=rec,tempPath=tempFasta,scrub=False)
+		manageTemp(record=rec, tempPath=tempFasta, scrub=False)
 		# Compose Nucmer script for current element vs self
-		runner = nucmer.Runner(tempFasta, tempFasta, tempCoords,
-						min_id=minid, min_length=minterm, 
-						breaklen=100, maxmatch=True, 
-						simplify=False)
+		runner = nucmer.Runner(	tempFasta, tempFasta, tempCoords,
+								min_id		=	minid,
+								min_length	=	minseed,
+								diagfactor	=	diagfactor,
+								incluster	=	minterm,
+								breaklen	=	200,
+								maxmatch	=	True,
+								simplify	=	False
+								)
 		# Execute nucmer
 		runner.run()
 		# Import coords file to iterator object
@@ -156,108 +187,164 @@ def getTIRs(elements=None,flankdist=10,minterm=10,minid=90,mites=False,report='s
 		# Filter for 5' repeats which begin within x bases of element start
 		alignments = [hit for hit in alignments if hit.ref_start <= flankdist]
 		# Scrub overlappying ref / query segments, and also complementary 3' to 5' flank hits
-		alignments = [hit for hit in alignments if hit.ref_end < hit.qry_start]
+		alignments = [hit for hit in alignments if hit.ref_end < hit.qry_end]
 		# Sort largest to smallest dist between end of ref (subject) and start of query (hit)
 		# x.qry_start - x.ref_end = length of internal segment
-		alignments = sorted(alignments, key=lambda x: (x.qry_start - x.ref_end), reverse=True)
+		alignments = sorted(alignments, key=lambda x: (x.qry_end - x.ref_end), reverse=True)
 		# If alignments exist after filtering report features using alignment pair with largest 
 		# internal segment i.e. first element in sorted list.
 		if alignments:
 			if report == 'all':
 				yield rec
 			if report in ['split','external']:
-				yield rec[alignments[0].ref_start:alignments[0].ref_end] # yield LTR slice - append "_TIR"
+				# yield TIR slice - append "_TIR"
+				extSeg = rec[alignments[0].ref_start:alignments[0].ref_end]
+				extSeg.id = extSeg.id + "_TIR"
+				extSeg.name = extSeg.id
+				yield extSeg
 			if report in ['split','internal']:
-				yield rec[alignments[0].ref_end:alignments[0].qry_start] # yield internal slice - append "_I"
+				# yield internal slice - append "_I"
+				intSeg = rec[alignments[0].ref_end:alignments[0].qry_end]
+				intSeg.id = intSeg.id + "_I"
+				intSeg.name = intSeg.id
+				yield intSeg
 			if mites:
-				# Reassemble TIRs into hypothetical MITEs
-				yield str(rec[alignments[0].ref_start:alignments[0].ref_end]) + str(rec[alignments[0].qry_start:alignments[0].qry_end])
+				# Assemble TIRs into hypothetical MITEs
+				synMITE = rec[alignments[0].ref_start:alignments[0].ref_end] + rec[alignments[0].qry_end:alignments[0].qry_start]
+				synMITE.id = synMITE.id + "_synMITE"
+				synMITE.name = synMITE.id
+				synMITE.description = "Synthetic MITE constructed from " + rec.id
+				yield synMITE
 		else:
 			# If alignment list empty after filtering print alert and continue
-			print('No LTRs found for candidate element: %s' % rec.id)
-			pass
+			print('No TIRs found for candidate element: %s' % rec.id)
 		# Scrub single fasta and coords file for current element.
-		manageTemp(tempPath=tempFasta,scrub=True)
-		manageTemp(tempPath=tempCoords,scrub=True)
+		manageTemp(tempPath=tempFasta, scrub=True)
+		manageTemp(tempPath=tempCoords, scrub=True)
 
-
-def segWrite(outhandle,segs=None):
-	"""..."""
+def segWrite(outfile,segs=None):
+	"""Take a generator object yielding seqrecords and write each to outfile in fasta format."""
 	if segs:
-		with open(outhandle, "w") as f:
+		with open(outfile, "w") as handle:
 			for seq in segs:
-				SeqIO.write(seq, 'fasta', f)
+				SeqIO.write(seq, handle, "fasta")
 	else:
+		print('No extracted segments to write. Finishing.')
 		pass
 
 def mainArgs():
 	parser = argparse.ArgumentParser(
-			description='Extract terminal repeats from retrotransposons (LTRs) or DNA transposons (TIRs). Compose synthetic MITES from complete DNA transposons.',
-			prog='TE-exterminate')
+							description	=	'Extract terminal repeats from retrotransposons (LTRs) or DNA transposons (TIRs). \
+											Optionally, compose synthetic MITES from complete DNA transposons.',
+							prog		=	'TE-exterminate'
+							)
 	parser.add_argument('-i', '--infile',
-							type=str,
-							required=True,
-							default=None,
-							help='Multifasta containing complete elements.'
+							type		=	str,
+							required	=	True,
+							default		=	None,
+							help		=	'Multifasta containing complete elements.'
 							)
 	parser.add_argument('-p', '--prefix',
-							type=str,
-							default='exterminate_split_repeats',
-							help='All output files begin with this string.'
+							type	=	str,
+							default	=	None,
+							help	=	'All output files begin with this string. \
+										(Default: [infile name])'
 							)
 	parser.add_argument('-d', '--outdir',
-							type=str,
-							default=None,
-							help='Write output files to this directory.'
+							type	=	str,
+							default	=	None,
+							help	=	'Write output files to this directory. \
+										(Default: cwd)'
 							)	
 	parser.add_argument('--findmode',
-							default='LTR',
-							choices=['LTR','TIR'],
-							help='Type of terminal repeat to identify. LTR or TIR.')
+							default	=	'LTR',
+							choices	=	['LTR','TIR'],
+							help	=	'Type of terminal repeat to identify. LTR or TIR. \
+										(Default: LTR)'
+							)
 	parser.add_argument('--splitmode',
-							default='split',
-							choices=['all','split','internal','flanks',None],
-							help='All= Report input sequence as well as internal and external segments. \
-							split= Report internal and external segments after splitting. \
-							internal = Report only internal segments \
-							flanks= Report only flanking segments.')
+							default	=	'split',
+							choices	=	['all','split','internal','external',None],
+							help	=	'all= Report input sequence as well as internal and external segments. \
+										split= Report internal and external segments after splitting. \
+										internal = Report only internal segments \
+										external = Report only terminal repeat segments. \
+										If set to "None" then only synthetic MITES will be reported if --makemites is also set. \
+										(Default: split)'
+							)
 	parser.add_argument('--makemites',
-							action='store_true',
-							default=False,
-							help='Attempt to construct synthetic MITE sequences from TIRs.')
+							action	=	'store_true',
+							default	=	False,
+							help	=	'Attempt to construct synthetic MITE sequences from TIRs.'
+							)
 	parser.add_argument('-m', '--maxdist',
-							type=int,
-							default=10,
-							help='Terminal repeat candidates must be no more than this many bases from end of input element.'
+							type	=	int,
+							default	=	10,
+							help	=	'Terminal repeat candidates must be no more than this many bases from end of input element. \
+										Note: Increase this value if you suspect that your element is nested within some flanking sequence.'
 							)
 	parser.add_argument('--minid',
-							type=float,
-							default=95.0,
-							help='Minimum identity between terminal repeat pairs. As float. Default: 95.0'
+							type	=	float,
+							default	=	80.0,
+							help	=	'Minimum identity between terminal repeat pairs. As float. \
+										(Default: 80.0)'
 							)
 	parser.add_argument('--minterm',
-							type=int,
-							default=10,
-							help='Minimum length for a terminal repeat to be considered.'
+							type	=	int,
+							default	=	10,
+							help	=	'Minimum length for a terminal repeat to be considered. \
+										Equivalent to nucmer "--mincluster" '
+							)
+	parser.add_argument('--minseed',
+							type	=	int,
+							default	=	5,
+							help	=	'Minimum length of an maximal exact match to be included in final match cluster. \
+										Equivalent to nucmer "--minmatch". \
+										(Default: 5)'
+							)
+	parser.add_argument('--diagfactor',
+							type	=	float,
+							default	=	0.2,
+							help	=	'Maximum diagonal difference factor for clustering of matches within nucmer, \
+										i.e. diagonal difference / match separation (default 0.20) \
+										Note: Increase value for greater tolerance of indels between terminal repeats.'
 							)
 	args = parser.parse_args()
 	return args
 
 def main(args):
-	outDir,tempdir = dochecks(args)
-
+	# Create output paths as required
+	outpath,tempdir = dochecks(args)
+	
+	# Load elements to be screened
 	elements = importFasta(args.infile)
-
-	outfile = args.prefix + "_exterm_extracted.fasta"
-	outpath = os.path.join(outDir,outfile)
-
+	
+	# If LTR mode, search for terminal repeats on same strand
 	if args.findmode == 'LTR':
-		segments = getLTRs(elements=elements,flankdist=args.maxdist,minterm=args.minterm,minid=args.minid,report=args.splitmode,temp=tempdir)
-		segWrite(outpath,segs=segments)
-	elif args.findmode == 'TIR':
-		segments = getTIRs(elements=elements,flankdist=args.maxdist,minterm=args.minterm,minid=args.minid,mites=args.makemites,report=args.splitmode,temp=tempdir)
-		segWrite(outpath,segs=segments)
-
+		segments = getLTRs( elements	=	elements,
+							flankdist	=	args.maxdist,
+							minterm		=	args.minterm,
+							minseed		=	args.minseed,
+							minid		=	args.minid,
+							diagfactor	=	args.diagfactor,
+							report		=	args.splitmode,
+							temp		=	tempdir
+							)
+		segWrite(outpath, segs=segments)
+	# If TIR mode or if makeMITE mode enabled, search for inverted terminal repeats
+	# Optionally construct synthetic MITE from TIRs if discovered
+	elif args.findmode == 'TIR' or args.makemites:
+		segments = getTIRs( elements	=	elements,
+							flankdist	=	args.maxdist,
+							minterm		=	args.minterm,
+							minseed		=	args.minseed,
+							minid		=	args.minid,
+							diagfactor	=	args.diagfactor,
+							mites		=	args.makemites,
+							report		=	args.splitmode,
+							temp		=	tempdir
+							)
+		segWrite(outpath, segs=segments)
 
 	# Remove temp directory
 	shutil.rmtree(tempdir)
@@ -265,12 +352,13 @@ def main(args):
 if __name__== '__main__':
 	args = mainArgs()
 	main(args)
+
 """
-##Filter for within x% of end
-[x.ref_start for x in alignments] #Note: Idexed from 1
-[x.ref_end for x in alignments] #Note: Idexed from 1
-[x.qry_start for x in alignments] #Note: Idexed from 1
-[x.qry_end for x in alignments] #Note: Idexed from 1
+# Useful attributes of pymummer objects:
+[x.ref_start for x in alignments]
+[x.ref_end for x in alignments]
+[x.qry_start for x in alignments]
+[x.qry_end for x in alignments]
 [x.hit_length_ref for x in alignments]
 [x.hit_length_qry for x in alignments]
 [x.percent_identity for x in alignments]
@@ -280,25 +368,9 @@ if __name__== '__main__':
 [x.ref_name for x in alignments]
 [x.qry_name for x in alignments]
 
+## Don't use these, bizzaro format. Not indexed to 0. Cannot sort as ints.
 #coord.reverse_query()
 #coord.reverse_reference()
 #coord.qry_coords()
 #coord.ref_coords()
-
 """
-"""
-fields = line.rstrip().split('\t')
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
